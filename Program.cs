20f14171c90f4a6e79401aa05fe80dc78ee95a18
Savanna Dickie <personal3@SavannaMac.local>
2024-11-09 15:45:56 -0700

    Preliminaries and story file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Savanna Dickie
+// 11/09/2024
+// Lab 9: Maze #2
+Console.Clear();
+Console.WriteLine("================================= MAZE #2 ================================");
+Console.WriteLine("\nObjective: Collect all 10 coins ( ^ ) to open the gate to gather gems ( $ ).");
+Console.WriteLine("\nChallange: Avoid the bad guys ( % ) to survive");
+Console.WriteLine("\n===============================GOOD LUCK ================================");
+Console.ReadLine();
+Console.Clear();
+Console.WriteLine("================================= MAZE #2 ================================");
