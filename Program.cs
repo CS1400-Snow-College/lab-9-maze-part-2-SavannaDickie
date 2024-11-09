9b07d73f7ddb1f393efaa8c2e7dec95d140eb5b1
Savanna Dickie <personal3@SavannaMac.local>
2024-11-09 16:31:38 -0700

    Basic User controls

--- Program.cs
+++ Program.cs
@@ -9,13 +9,28 @@ Console.WriteLine("\nObjective: Collect all 10 coins ( ^ ) to open the gate to g
 Console.WriteLine("\nChallange: Avoid the bad guys ( % ) to survive");
 Console.WriteLine("\n===============================GOOD LUCK ================================");
 Console.ReadLine();
-Console.Clear();
+
 
 
 string[] mapRows = File.ReadAllLines("maze.txt");
 Console.Clear();
-Console.WriteLine("================================= MAZE #2 ================================");
+//Console.WriteLine("================================= MAZE #2 ================================");
 foreach (string row in mapRows)
 {
     Console.WriteLine(row);
 }
+Console.SetCursorPosition(0,0);
+do
+{
+    ConsoleKey Key = Console.ReadKey(true).Key;
+    if (Key == ConsoleKey.Escape) {return;}
+    int cursorTop = Console.CursorTop;
+    int cursorLeft = Console.CursorLeft;
+
+    if (Key == ConsoleKey.UpArrow) {cursorTop--;}
+    else if (Key == ConsoleKey.DownArrow) {cursorTop ++;}
+    else if (Key == ConsoleKey.LeftArrow) {cursorLeft--;}
+    else if (Key == ConsoleKey.RightArrow) {cursorLeft++;}
+    Console.SetCursorPosition(cursorLeft, cursorTop);
+    
+} while(true);
